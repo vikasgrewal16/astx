1927c57a14760f56b704ade75a2130912496bc1d
vikasgrewal16 <vv172002kumar@gmail.com>
2024-10-02 00:32:27 +0530

    Adding transpilers for float and complex

--- tests/transpilers/test_python.py
+++ tests/transpilers/test_python.py
@@ -59,3 +59,59 @@ def test_function() -> None:
     )
 
     assert generated_code == expected_code, "generated_code != expected_code"
+
+
+def test_float16() -> None:
+    """Test astx.float."""
+    # Function parameters
+    args = astx.Arguments(
+        astx.Argument(name="x", type_=astx.Float16),
+        astx.Argument(name="y", type_=astx.Float16),
+    )
+
+    # Function body
+    body = astx.Block()
+    body.append(
+        astx.VariableAssignment(
+            name="result",
+            value=astx.BinaryOp(
+                op_code="+",
+                lhs=astx.Variable(name="x"),
+                rhs=astx.Variable(name="y"),
+                loc=astx.SourceLocation(line=2, col=8),
+            ),
+            loc=astx.SourceLocation(line=2, col=4),
+        )
+    )
+    body.append(
+        astx.FunctionReturn(
+            value=astx.Variable(name="result"),
+            loc=astx.SourceLocation(line=3, col=4),
+        )
+    )
+
+    # Function definition
+    add_function = astx.Function(
+        prototype=astx.FunctionPrototype(
+            name="add",
+            args=args,
+            return_type=astx.Float16,
+        ),
+        body=body,
+        loc=astx.SourceLocation(line=1, col=0),
+    )
+
+    # Initialize the generator
+    generator = astx2py.ASTxPythonTranspiler()
+
+    # Generate Python code
+    generated_code = generator.visit(add_function)
+    expected_code = "\n".join(
+        [
+            "def add(x: float, y: float) -> float:",
+            "    result = (x + y)",
+            "    return result",
+        ]
+    )
+
+    assert generated_code == expected_code, "generated_code != expected_code"
